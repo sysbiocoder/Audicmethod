6e8524a8436e8a4b11fe92e132a35a55e1c4d27b
sysbiocoder <subazinitk@gmail.com>
2016-11-17 15:58:24 +0100

    Update audic.R

--- audic.R
+++ audic.R
@@ -1,10 +1,12 @@
 #This is differential expression analysis for RNA sequencing data with no replicates using Audic and Claverie method 1997
 
-# To run from command line 
+
 
 # Input file
-#The input file is Genenames, counts and length columns separated by <TAB> space
+#The input file should comtain Genenames followed by counts and length columns separated by <TAB> space. 
+#Note length column should be at the end.
 
+# To run from command line 
 # use  R CMD BATCH  '--args ctrl_column_no tmt_column_no input_file outfolder_name Length_column_no audic.R audic.Rout
 
 
